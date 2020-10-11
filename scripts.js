fd4dfe145938e7c57d75c5bbc882a3ac501a93f0
dhintz <dhintz89@gmail.com>
2020-10-11 17:39:25 -0400

    add digital window to clock and gold accent colors

--- scripts.js
+++ scripts.js
@@ -1,12 +1,36 @@
 function getTime() {
     date = new Date();
-    return {hour: date.getHours(), minute: date.getMinutes(), second: date.getSeconds()};
+    switch (date.getDay()) {
+        case 0:
+            day = "Sun";
+            break;
+        case 1:
+            day = "Mon";
+            break;
+        case 2:
+            day = "Tue";
+            break;
+        case 3:
+            day = "Wed";
+            break;
+        case 4:
+            day = "Thu";
+            break;
+        case 5:
+            day = "Fri";
+            break;
+        case 6:
+            day = "Sat";
+            break;
+    }
+    return {day: day, hour: date.getHours(), minute: date.getMinutes(), second: date.getSeconds()};
 }
 
 function tick() {
     const hourHand = document.querySelector(".hour");
     const minHand = document.querySelector(".minute");
     const secHand = document.querySelector(".second");
+    const digitalClock = document.querySelector(".digWindow");
     time = getTime()
     
     // hour hand
@@ -19,6 +43,9 @@ function tick() {
     minHand.style.transform = `rotate(${(360 / 60) * time.minute}deg)`;
     // second hand
     secHand.style.transform = `rotate(${(360 / 60) * time.second}deg)`;
+
+    // digital window
+    digitalClock.innerHTML = `${time.day} - ${time.hour} : ${time.minute}`;
 }
 
 let interval = setInterval(tick, 1000);
